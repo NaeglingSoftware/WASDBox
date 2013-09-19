5820e0275559602bb01fdb72a6962dff996208df
petermenke <peter.menke22@gmail.com>
2013-09-19 16:35:26 -0700

    Box cannot go off the screen

--- src/main/java/DisplayTester.java
+++ src/main/java/DisplayTester.java
@@ -3,53 +3,63 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.*;
 
 public class DisplayTester {
-	public void start() {
+    public void start() {
 
-		try {
-			Display.setDisplayMode(new DisplayMode(800, 600));
-			Display.create();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
+        try {
+            Display.setDisplayMode(new DisplayMode(800, 600));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
 
-		GL11.glMatrixMode(GL11.GL_PROJECTION);
-		GL11.glLoadIdentity();
-		GL11.glOrtho(0, 800, 0, 600, 1, -1);
-		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, 800, 0, 600, 1, -1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
-		while (!Display.isCloseRequested()) {
-			drawStuff();
-			Display.update();
-		}
-		Display.destroy();
-	}
+        while (!Display.isCloseRequested()) {
+            drawStuff();
+            Display.update();
+        }
+        Display.destroy();
+    }
 
-	int x = 100, y = 100, size = 25;
+    int x = 100, y = 100, size = 25;
 
-	public void drawStuff() {
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-		GL11.glColor3f(0.5f, 0.5f, 1.0f);
+    public void drawStuff() {
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+        GL11.glColor3f(0.5f, 0.5f, 1.0f);
 
-		Keyboard.enableRepeatEvents(true);
+        Keyboard.enableRepeatEvents(true);
 
-		while (Keyboard.next()) {
-			if (Keyboard.getEventKeyState()) {
-				switch (Keyboard.getEventKey()) {
-					case Keyboard.KEY_W: y+=10; break;
-					case Keyboard.KEY_A: x-=10; break;
-					case Keyboard.KEY_S: y-=10; break;
-					case Keyboard.KEY_D: x+=10; break;
-					case Keyboard.KEY_Q: size++; break;
-					case Keyboard.KEY_E: size--; break;
-				}
-			}
-		}
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2f(x - size, y - size);
-		GL11.glVertex2f(x - size, y + size);
-		GL11.glVertex2f(x + size, y + size);
-		GL11.glVertex2f(x + size, y - size);
-		GL11.glEnd();
-	}
+        while (Keyboard.next()) {
+            if (Keyboard.getEventKeyState()) {
+                switch (Keyboard.getEventKey()) {
+                    case Keyboard.KEY_W: y+=10; break;
+                    case Keyboard.KEY_A: x-=10; break;
+                    case Keyboard.KEY_S: y-=10; break;
+                    case Keyboard.KEY_D: x+=10; break;
+                    case Keyboard.KEY_Q: size++; break;
+                    case Keyboard.KEY_E: size--; break;
+                }
+            }
+        }
+
+        if (y < size)
+            y = size;
+        else if (x < size)
+            x = size;
+        else if (y > 600 - size)
+            y = 600 - size;
+        else if (x > 800 - size)
+            x = 800 - size;
+
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex2f(x - size, y - size);
+        GL11.glVertex2f(x - size, y + size);
+        GL11.glVertex2f(x + size, y + size);
+        GL11.glVertex2f(x + size, y - size);
+        GL11.glEnd();
+    }
 }
