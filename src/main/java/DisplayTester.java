5b9d0c477fa212382f619ae64e4f790bc253a3cf
petermenke <peter.menke22@gmail.com>
2013-09-20 17:55:52 -0700

    Code refactorization

--- src/main/java/DisplayTester.java
+++ src/main/java/DisplayTester.java
@@ -3,6 +3,9 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.*;
 
 public class DisplayTester {
+
+	Quad WASDBox = new Quad(400, 300, 25, 0);
+
 	public void start() {
 
 		try {
@@ -18,6 +21,8 @@ public class DisplayTester {
 		GL11.glOrtho(0, 800, 0, 600, 1, -1);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
+		Keyboard.enableRepeatEvents(true);
+
 		while (!Display.isCloseRequested()) {
 			drawStuff();
 			Display.update();
@@ -25,48 +30,11 @@ public class DisplayTester {
 		Display.destroy();
 	}
 
-	int x = 100, y = 100, size = 25, rotation = 0;
-
 	public void drawStuff() {
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 		GL11.glColor3f(0.5f, 0.5f, 1.0f);
 
-		Keyboard.enableRepeatEvents(true);
-
-		while (Keyboard.next()) {
-			if (Keyboard.getEventKeyState()) {
-				switch (Keyboard.getEventKey()) {
-					case Keyboard.KEY_W: y+=10; break;
-					case Keyboard.KEY_A: x-=10; break;
-					case Keyboard.KEY_S: y-=10; break;
-					case Keyboard.KEY_D: x+=10; break;
-					case Keyboard.KEY_Q: size++; break;
-					case Keyboard.KEY_E: size--; break;
-					case Keyboard.KEY_Z: rotation += 5; break;
-					case Keyboard.KEY_C: rotation -= 5; break;
-				}
-			}
-		}
-
-		if (y < size)
-			y = size;
-		else if (x < size)
-			x = size;
-		else if (y > 600 - size)
-			y = 600 - size;
-		else if (x > 800 - size)
-			x = 800 - size;
-
-		GL11.glPushMatrix();
-			GL11.glTranslatef(x, y, 0);
-			GL11.glRotatef(rotation, 0f, 0f, 1f);
-			GL11.glTranslatef(-x, -y, 0);
-			GL11.glBegin(GL11.GL_QUADS);
-				GL11.glVertex2f(x - size, y - size);
-				GL11.glVertex2f(x + size, y - size);
-				GL11.glVertex2f(x + size, y + size);
-				GL11.glVertex2f(x - size, y + size);
-			GL11.glEnd();
-		GL11.glPopMatrix();
+		WASDBox.update();
+		WASDBox.display();
 	}
 }
---
+++ src/main/java/Quad.java
@@ -0,0 +1,89 @@
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;
+
+public class Quad {
+
+	private int x;
+	private int y;
+	private int size;
+	private int rotation;
+
+	public Quad(int xCoord, int yCoord, int s, int r) {
+		x = xCoord;
+		y = yCoord;
+		size = s;
+		rotation = r;
+	}
+
+	public void moveUp(int num) {
+		y += num;
+	}
+
+	public void moveDown(int num) {
+		y -= num;
+	}
+
+	public void moveLeft(int num) {
+		x -= num;
+	}
+
+	public void moveRight(int num) {
+		x += num;
+	}
+
+	public void grow(int num) {
+		size += num;
+	}
+
+	public void shrink(int num) {
+		size -= num;
+	}
+
+	public void rotate(int num){
+		rotation += num;
+	}
+
+	public void update() {
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState()) {
+				switch (Keyboard.getEventKey()) {
+					case Keyboard.KEY_W: moveUp(10); break;
+					case Keyboard.KEY_A: moveLeft(10); break;
+					case Keyboard.KEY_S: moveDown(10); break;
+					case Keyboard.KEY_D: moveRight(10); break;
+					case Keyboard.KEY_Q: grow(1); break;
+					case Keyboard.KEY_E: shrink(1); break;
+					case Keyboard.KEY_Z: rotate(5); break;
+					case Keyboard.KEY_C: rotate(-5); break;
+				}
+			}
+		}
+	}
+
+	public void checkBoundaries() {
+		if (y < size)
+			y = size;
+		else if (x < size)
+			x = size;
+		else if (y > 600 - size)
+			y = 600 - size;
+		else if (x > 800 - size)
+			x = 800 - size;
+	}
+
+	public void display() {
+		checkBoundaries();
+
+		GL11.glPushMatrix();
+		GL11.glTranslatef(x, y, 0);
+		GL11.glRotatef(rotation, 0f, 0f, 1f);
+		GL11.glTranslatef(-x, -y, 0);
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex2f(x - size, y - size);
+		GL11.glVertex2f(x + size, y - size);
+		GL11.glVertex2f(x + size, y + size);
+		GL11.glVertex2f(x - size, y + size);
+		GL11.glEnd();
+		GL11.glPopMatrix();
+	}
+}
