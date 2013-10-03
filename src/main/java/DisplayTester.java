bbf05dc98d4eb3798c7f50337ee3862f856be068
petermenke <peter.menke22@gmail.com>
2013-10-03 08:46:29 -0700

    Remove unnecessary comment

--- src/main/java/DisplayTester.java
+++ src/main/java/DisplayTester.java
@@ -21,8 +21,6 @@ public class DisplayTester {
 		GL11.glOrtho(0, 800, 0, 600, 1, -1);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
-		//Keyboard.enableRepeatEvents(true);
-
 		while (!Display.isCloseRequested()) {
 			drawStuff();
 			Display.update();
